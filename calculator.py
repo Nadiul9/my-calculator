cdb9d85456cb00c655656105101242503ccaee24
Nadiul <nadiulislam614@gmail.com>
2026-03-10 16:54:29 +0530

    Day 1- Calculator Working!

--- calculator.py
+++ calculator.py
@@ -1,17 +1,24 @@
-def calculator ():
-    print("=== Simple calculator ===")
-    op =float (input("Enter first number: "))
-    num2 = float(input("Enter second number:"))
-    if op == '+': result = num1 + num2
-    elif op=='-' : result = num1 - num2
-    elif op =='*': result = num1 * num2
-       if num2 == 0:
-        print("error: Division by zero!")
+def calculator():
+    print("=== Simple Calculator ===")
+    num1 = float(input("Enter first number: "))
+    op = input("Enter operator (+, -, *, /): ")
+    num2 = float(input("Enter second number: "))
+    
+    if op == '+':
+        result = num1 + num2
+    elif op == '-':
+        result = num1 - num2
+    elif op == '*':
+        result = num1 * num2
+    elif op == '/':
+        if num2 == 0:
+            print("Error: Division by zero!")
+            return
+        result = num1 / num2
+    else:
+        print("Invalid operator!")
         return
-    result = num1 / num2
-else:
-print("Invalid operator!")
-return 
-print (f"Result:{num1} {op} {num2} = {result}
-       calculator()
-       
+    
+    print(f"Result: {num1} {op} {num2} = {result}")
+
+calculator()
